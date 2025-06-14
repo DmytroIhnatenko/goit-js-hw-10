4e39d7c583a266d863b1407a71e84522235f2759
dmytro_ihnatenko <ihnatenkodmytro0@gmail.com>
2025-06-14 02:39:58 +0200

    t

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "src",
+  base: '/goit-js-hw-10/',
   build: {
-    outDir: "../dist",
-    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        timer: 'src/1-timer.html',
+        snackbar: 'src/2-snackbar.html',
+      },
+    },
   },
 });
